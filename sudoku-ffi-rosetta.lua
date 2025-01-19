9993ac360ec7dae42e121e02a1663783126ca949
Martin-Louis Bright <mlbright@gmail.com>
2025-01-19 15:32:45 -0500

    slightly faster

--- sudoku-ffi-rosetta.lua
+++ sudoku-ffi-rosetta.lua
@@ -2,11 +2,14 @@
 
 -- From Rosetta Code: https://rosettacode.org/wiki/Sudoku#with_FFI,_fast
 
-ffi = require("ffi")
+FFI = require("ffi")
+
 local printf = function(fmt, ...)
   io.write(string.format(fmt, ...))
 end
+
 local band, bor, lshift, rshift = bit.band, bit.bor, bit.lshift, bit.rshift
+
 local function show(x)
   for i = 0, 8 do
     if i % 3 == 0 then
@@ -18,29 +21,37 @@ local function show(x)
     print()
   end
 end
+
 local function trycell(x, pos)
-  local row = math.floor(pos / 9)
-  local col = pos % 9
-  local used = 0
   if pos == 81 then
     return true
   end
+
+  local row = math.floor(pos / 9)
+  local col = pos % 9
+  local used = 0
+
   if x[pos] ~= 0 then
     return trycell(x, pos + 1)
   end
+
   for i = 0, 8 do
     used = bor(used, lshift(1, x[i * 9 + col] - 1))
   end
+
   for j = 0, 8 do
     used = bor(used, lshift(1, x[row * 9 + j] - 1))
   end
+
   row = math.floor(row / 3) * 3
   col = math.floor(col / 3) * 3
+
   for i = row, row + 2 do
     for j = col, col + 2 do
       used = bor(used, lshift(1, x[i * 9 + j] - 1))
     end
   end
+
   x[pos] = 1
   while x[pos] <= 9 do
     if band(used, 1) == 0 and trycell(x, pos + 1) then
@@ -49,15 +60,18 @@ local function trycell(x, pos)
     used = rshift(used, 1)
     x[pos] = x[pos] + 1
   end
+
   x[pos] = 0
   return false
 end
+
 local function solve(str)
-  local x = ffi.new("char[?]", 81)
+  local x = FFI.new("char[?]", 81)
   str = str:gsub("[%c%s]", "")
   for i = 0, 81 do
     x[i] = tonumber(str:sub(i + 1, i + 1)) or 0
   end
+
   if trycell(x, 0) then
     show(x)
   else
